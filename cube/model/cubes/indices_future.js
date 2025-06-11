fcb925c892cbdc0dcbb50016100e9f10637111d2
DuyLVD <luongvudinhduy03@gmail.com>
2025-06-11 17:03:19 +0700

    .

---
+++ cube/model/cubes/indices_future.js
@@ -0,0 +1,93 @@
+cube(`indices_future`, {
+  sql_table: `gold.indices_future`,
+  
+  data_source: `default`,
+  
+  joins: {
+    
+  },
+  
+  dimensions: {
+    id: {
+      sql: `id`,
+      type: `number`,
+      primary_key: true
+    },
+    
+    gold_srvar: {
+      sql: `gold_srvar`,
+      type: `number`
+    },
+    
+    russell2000_srvar: {
+      sql: `russell2000_srvar`,
+      type: `number`
+    },
+    
+    dow_jones_srvar: {
+      sql: `dow_jones_srvar`,
+      type: `number`
+    },
+    
+    msci_world_srvar: {
+      sql: `msci_world_srvar`,
+      type: `number`
+    },
+    
+    nasdaq100_srvar: {
+      sql: `nasdaq100_srvar`,
+      type: `number`
+    },
+    
+    s_p500_srvar: {
+      sql: `s_p500_srvar`,
+      type: `number`
+    },
+    
+    gold_varnn: {
+      sql: `gold_varnn`,
+      type: `number`
+    },
+    
+    russell2000_varnn: {
+      sql: `russell2000_varnn`,
+      type: `number`
+    },
+    
+    dow_jones_varnn: {
+      sql: `dow_jones_varnn`,
+      type: `number`
+    },
+    
+    msci_world_varnn: {
+      sql: `msci_world_varnn`,
+      type: `number`
+    },
+    
+    nasdaq100_varnn: {
+      sql: `nasdaq100_varnn`,
+      type: `number`
+    },
+    
+    s_p500_varnn: {
+      sql: `s_p500_varnn`,
+      type: `number`
+    },
+    
+    date: {
+      sql: `date`,
+      type: `time`
+    }
+  },
+  
+  measures: {
+    count: {
+      type: `count`
+    }
+  },
+  
+  pre_aggregations: {
+    // Pre-aggregation definitions go here.
+    // Learn more in the documentation: https://cube.dev/docs/caching/pre-aggregations/getting-started
+  }
+});
---
+++ cube/model/cubes/macro_eval.js
@@ -0,0 +1,63 @@
+cube(`macro_eval`, {
+  sql_table: `gold.macro_eval`,
+  
+  data_source: `default`,
+  
+  joins: {
+    
+  },
+  
+  dimensions: {
+    id: {
+      sql: `id`,
+      type: `number`,
+      primary_key: true
+    },
+    
+    variable: {
+      sql: `variable`,
+      type: `string`
+    },
+    
+    mse_srvar: {
+      sql: `mse_srvar`,
+      type: `number`
+    },
+    
+    rmse_srvar: {
+      sql: `rmse_srvar`,
+      type: `number`
+    },
+    
+    mae_srvar: {
+      sql: `mae_srvar`,
+      type: `number`
+    },
+    
+    mse_var: {
+      sql: `mse_var`,
+      type: `number`
+    },
+    
+    rmse_var: {
+      sql: `rmse_var`,
+      type: `number`
+    },
+    
+    mae_var: {
+      sql: `mae_var`,
+      type: `number`
+    }
+  },
+  
+  measures: {
+    count: {
+      type: `count`
+    }
+  },
+  
+  pre_aggregations: {
+    // Pre-aggregation definitions go here.
+    // Learn more in the documentation: https://cube.dev/docs/caching/pre-aggregations/getting-started
+  }
+});
